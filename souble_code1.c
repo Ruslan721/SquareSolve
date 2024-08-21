9f859701302b71c72ae44e2b9fdb8da0fb82a311
Ruslan721 <112270234+Ruslan721@users.noreply.github.com>
2024-08-21 20:39:06 +0300

    Рабочие программы тестировщики с вводом и без, исправлена архитектура вызова функций (ввод -> счёт -> вывод)

--- souble_code1.c
+++ souble_code1.c
@@ -2,69 +2,62 @@
 #include <math.h>
 #include <locale.h>
 
+const int INF = -1;
+
 const double e = 1e-20;
 
-void InputFunc(double* a, double* b, double* c);
+void InputFunc();
 int SqrSolution(double a, double b, double c, double* x1, double* x2);
+void OutputFun_SS(const int res,const double x1,const double x2);
 double Diskr(double a, double b, double c);
 int DoubleComp(double a, double b);
 int Restart(int* key);
-/*int RunTest(double a, double b, double c, double x1_exp, double x2_exp, int nRoots_exp);
-void InputFunc_RT(double* a, double* b, double* c, double* x1_exp, double* x2_exp, int* nRoots_exp);*/
+void RunTest(double a, double b, double c, double x1_exp, double x2_exp, int nRoots_exp);
+void InputFunc_RT();
+void InputTests_RT();
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
-     /*
-    double a = 0, b = 0, c = 0, x1_exp = 0, x2_exp = 0;         // тест
-    int nRoots_exp = 0;
-    InputFunc_RT(&a, &b, &c, &x1_exp, &x2_exp, &nRoots_exp);
-    RunTest(a, b, c, x1_exp, x2_exp, nRoots_exp); */
 
 
+   //InputFunc_RT();
+    //InputTests_RT();       // тест
+
+
+    InputFunc();
+
+
+    return 0;
+}
+void InputFunc()
+{
     int key = 1;
     while (key == 1 || key == 0)
     {
         if (key == 0)
             break;
         key = -1;
-
         double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+        int num =0;
 
-        InputFunc(&a,&b,&c);
+        while (num != 3)
+        {
+            printf( "Введите значения коэффициэнтов\n"
+                    "квадратного уравнения: ");
+            num = scanf("%lg %lg %lg", &a, &b, &c);
+
+            if (num != 3)
+            while(getchar() != '\n') {}
+        }
 
         int res = SqrSolution(a, b,  c, &x1, &x2);
 
-        switch (res)
-        {
-        case 0: printf("Нет решений\n\n");
-                break;
-
-        case 1: printf("Корень уравнения: %.2lg\n\n",x1);
-                break;
-
-        case 2: printf("Корни уравнения: %.2lg, %.2lg\n\n", x1, x2);
-                break;
-
-        default: printf("error");
-        }
+        OutputFun_SS(res, x1, x2);
 
         Restart(&key);
     }
-        return 0;
-}
-void InputFunc( double* const a, double* const b, double* const c)
-{
-    int num =0;
 
-    while (num != 3)
-    {
-    printf("Введите значения коэффициэнтов\n"
-           "квадратного уравнения: ");
-    num = scanf("%lg %lg %lg", a, b, c);
-
-    while(getchar() != '\n') {}   //?
-    }
 }
 
 int SqrSolution(const double a,const double b,const double c, double* const x1, double* const x2)
@@ -74,14 +67,14 @@ int SqrSolution(const double a,const double b,const double c, double* const x1, 
         double d = Diskr(a,b,c);
         if (DoubleComp(d,0))
         {
-            *x1 = *x2 = -(b / 2 / a);
+            *x1 = *x2 = -(b /( 2 * a));
             return 1;
         }
         else if (d > 0)
         {
             d = sqrt(d)/2/a;
-            *x1 = -b/2/a + d;
-            *x2 = - b/2/a - d;
+            *x1 = -b/2/a - d;
+            *x2 = - b/2/a + d;
             return 2;
         }
         else
@@ -89,10 +82,14 @@ int SqrSolution(const double a,const double b,const double c, double* const x1, 
             return 0;
         }
     }
-    else
+    else if (!(DoubleComp(b,0)))
     {
         *x1 = *x2 = -c / b;
         return 1;
+    }
+    else
+    {
+        return INF;
     }
 }
 
@@ -101,9 +98,29 @@ double Diskr(const double a,const double b,const double c)
     return b*b - 4*a*c;
 }
 
+void OutputFun_SS(const int res,const double x1,const double x2)
+{
+    switch (res)
+    {
+    case 0: printf("Нет решений\n\n");
+            break;
+
+    case 1: printf("Корень уравнения: %.2lg\n\n",x1);
+            break;
+
+    case 2: printf("Корни уравнения: %.2lg, %.2lg\n\n", x1, x2);
+            break;
+
+    case INF: printf("Бесконечность решений\n");
+            break;
+
+    default: printf("error\n");
+    }
+}
+
 int DoubleComp(const double a, const double b)
 {
-    return fabs(a - b) > e;
+    return fabs(a - b) < e;
 }
 
 int Restart( int* const key)
@@ -112,35 +129,79 @@ int Restart( int* const key)
     {
     printf("Продолжть[ 1 ]\n"
             "Остановить[ 0 ]\n");
-    scanf("%d", key);
     while(getchar() != '\n') {}
+    int nkey = scanf("%d", key);
     }
 
     return 0;
 }
 
-/*int RunTest(const double a,const double b,const double c,const double x1_exp,const double x2_exp,const int nRoots_exp)    //тест
+void RunTest(const double a,const double b,const double c,const double x1_exp,const double x2_exp,const int nRoots_exp)    //тест
 {
     double x1 = 0, x2 = 0;
     int nRoots = SqrSolution(a,b,c,&x1, &x2);
-    if (x1 != x1_exp || x2 != x2_exp || nRoots != nRoots_exp)
+
+    if (!(DoubleComp(x1,x1_exp)) || !(DoubleComp(x2,x2_exp)) || !(DoubleComp(nRoots,nRoots_exp)))
     {
-        printf("Error\na = %.2lg, b = %.2lg, c = %.2lg\n"
-                "      x1    x2\nExp   %-6.2lg%.2lg\nRes   %-6.2lg%.2lg\n", a, b, c, x1_exp, x2_exp, x1, x2);
+        printf("\nError\n------------------\na = %.2lg, b = %.2lg, c = %.2lg\n"
+                "      x1    x2\nExp   %-6.2lg%.2lg\nRes   %-6.2lg%.2lg\n------------------\n", a, b, c, x1_exp, x2_exp, x1, x2);  //\t
     }
     else
     {
-        printf("perfect!\n");
+        printf("\nperfect!\n\n");
+    }
+
+}
+
+void InputTests_RT()
+{
+
+    printf("test 1\n");
+    RunTest(1,-2,1,1,1,1);
+
+    printf("test 2\n");
+    RunTest(1,-4,4,2,2,1);
+
+    printf("test 3\n");
+    RunTest(1,-3,2,1,2,2);
+
+    printf("test 4\n");
+    RunTest(0,1,-1,1,1,1);
+
+    printf("test 5\n");
+    RunTest(0,0,1,0,0,INF);
+}
+
+void InputFunc_RT()
+{
+    int key = 1;
+    while (key == 1 || key == 0)
+    {
+        if (key == 0)
+            break;
+        key = -1;
+
+        double a = 0, b = 0, c = 0, x1_exp = 0, x2_exp = 0;
+        int nRoots_exp = 0;
+
+        printf("Введите три коэффициента квадратного уравнения,\n"
+                "верные корни этого уравнения и количество различных\n"
+                "значений корня последовательно: ");
+        scanf("%lg %lg %lg %lg %lg %d", &a, &b, &c, &x1_exp, &x2_exp, &nRoots_exp);
+
+        RunTest(a, b, c, x1_exp, x2_exp, nRoots_exp);
+
+        Restart(&key);
     }
 }
 
-void InputFunc_RT( double* const a, double* const b, double* const c, double* const x1_exp, double* const x2_exp, int* const nRoots_exp)
-{
-    printf("Введите три коэффициента квадратного уравнения,\n"
-            "верные корни этого уравнения и количество различных\n"
-            "значений корня последовательно: ");
-    scanf("%lg %lg %lg %lg %lg %d", a, b, c, x1_exp, x2_exp, nRoots_exp);
-} */
 
 
-//оптимизировать ввод
+//оптимизировать ввод (ввод -> счёт -> вывод), цикл в вводе   \/
+//чтение файла
+//текстовый ввод
+//NUN, NULL
+//структуры
+//енам (enum)
+//doxygen
+//тесты с массивом
